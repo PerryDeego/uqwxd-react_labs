706c82cc3108be59743e5e2db4a3b2eac5b0ee85
PerryDeego <38059386+PerryDeego@users.noreply.github.com>
2024-06-19 22:32:45 -0500

    Create conter Component

--- react-redux-master/src/components/DivPanel.js
+++ react-redux-master/src/components/DivPanel.js
@@ -1,1 +1,14 @@
+import React from "react";
+import { useSelector } from "react-redux";
 
+const DivPanel = () => {
+    let counterVal = useSelector(state => state.counter);
+
+    return (
+        <div className="show-counter">
+            The  present value of the counte is: { counterVal }
+        </div>
+    );
+}
+
+export default DivPanel
