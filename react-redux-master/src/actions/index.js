26cd5d191143f27d2af64a3e351bda721ce692a6
PerryDeego <38059386+PerryDeego@users.noreply.github.com>
2024-06-19 22:31:17 -0500

    add increment action

--- react-redux-master/src/actions/index.js
+++ react-redux-master/src/actions/index.js
@@ -1,1 +1,14 @@
-//actions
+// actions INCREMENT
+
+const increment = (val) => {
+    return {
+        type: 'INCREMENT',
+        inc: val
+    }
+}
+
+// MyButton DivPanel for value
+
+export default increment;
+
+
